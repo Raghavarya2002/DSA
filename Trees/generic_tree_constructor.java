484d25d8a4d2bb517501658f0b4a58969a4b9714
Raghav arya <68361729+Raghavarya2002@users.noreply.github.com>
2022-03-22 13:44:03 +0530

    Update generic_tree_constructor.java

--- Trees/generic_tree_constructor.java
+++ Trees/generic_tree_constructor.java
@@ -2,14 +2,27 @@ import java.util.*;
   
   public class Main{
       
-      private class Node{
+      private static class Node{
           int data;
-          ArrayList<Node> children = new Arraylist<>();
+          ArrayList<Node> children = new ArrayList<>();
+      }
+      
+      public static void display(Node node){
+          String str = node.data + " -> ";
+          for(Node child: node.children){
+              str += child.data + ", ";
+          }
+          
+          str+=".";
+          System.out.println(str);
+          for(Node child : node.children){
+              display(child);
+          }
       }
   
   public static void main(String[] args) {
       int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
-      
+      Node root = null;
       Stack<Node> st = new Stack<>();
       for(int i =0; i< arr.length; i++){
           if(arr[i] == -1){
@@ -29,6 +42,7 @@ import java.util.*;
               st.push(t);
           }
       }
+      display(root);
       
    }
   
