0adfb074d8fd49edbcd1ab1425ccd8476e947ae9
Raghav arya <68361729+Raghavarya2002@users.noreply.github.com>
2022-03-25 22:29:46 +0530

    Update Largest_Area_Histogram.java

--- Stack/Largest_Area_Histogram.java
+++ Stack/Largest_Area_Histogram.java
@@ -10,64 +10,63 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
+  
 
-  public static void main(String[] args) throws Exception {
+public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     int n = Integer.parseInt(br.readLine());
     int[] arr = new int[n];
-    for (int i = 0; i < n; i++) {
-      arr[i] = Integer.parseInt(br.readLine());
+    for(int i = 0; i < n; i++){
+       arr[i] = Integer.parseInt(br.readLine());
     }
 
-    int[] rb = new int[arr.length]; // next smaller element index on the right
+    // code
+    int [] rightboundary = new int[arr.length];  // next smaller element index on the right
     Stack<Integer> st = new Stack<>();
-
-    st.push(arr.length - 1);
-    rb[arr.length - 1] = arr.length;
-    for (int i = arr.length - 2; i >= 0; i--) {
-      while (st.size() > 0 && arr[i] <= arr[st.peek()]) {
-        st.pop();
-      }
-
-      if (st.size() == 0) {
-        rb[i] = arr.length;
-      } else {
-        rb[i] = st.peek();
-      }
-
-      st.push(i);
+    st.push(arr.length-1);
+    rightboundary[arr.length-1] = arr.length;
+    for(int i = arr.length-2 ; i>=0; i--){
+        while(st.size() > 0 && arr[i] <= arr[st.peek()]){
+            st.pop();
+        }
+        if(st.size() == 0){
+            rightboundary[i]  = arr.length;
+        }else{
+            rightboundary[i] = st.peek();
+        }
+        st.push(i);
     }
-
-    int[] lb = new int[arr.length]; // next smaller element index on the left
+    
+     int [] leftboundary = new int[arr.length]; // next smaller element index on the left
+    
+    
     st = new Stack<>();
-
     st.push(0);
-    lb[0] = -1;
-    for (int i = 1; i < arr.length; i++) {
-      while (st.size() > 0 && arr[i] <= arr[st.peek()]) {
-        st.pop();
-      }
-
-      if (st.size() == 0) {
-        lb[i] = -1;
-      } else {
-        lb[i] = st.peek();
-      }
-
-      st.push(i);
+    leftboundary[0] = -1;
+    for(int i = 1 ; i < arr.length; i++){
+        while(st.size() > 0 && arr[i] <= arr[st.peek()]){
+            st.pop();
+        }
+        if(st.size() == 0){
+            leftboundary[i]  = -1;
+        }else{
+            leftboundary[i] = st.peek();
+        }
+        st.push(i);
     }
-
-    int max = 0;
-    for (int i = 0; i < arr.length; i++) {
-      int width = rb[i] - lb[i] - 1;
-      int area = width * arr[i];
-      if (area > max) {
-        max = area;
-      }
+    
+    int maxArea =0;
+    for(int i =0;i < arr.length;i++){
+        int width = rightboundary[i] - leftboundary[i];
+        int area = arr[i] * width;
+        if(area > maxArea){
+            maxArea = area;
+        }
     }
-
-    System.out.println(max);
-  }
+    
+    System.out.println(maxArea);
+  
+ }
 }
