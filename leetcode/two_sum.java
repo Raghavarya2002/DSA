00b7aa9ce84b95405213dede5c5365da9889e324
Raghav arya <68361729+Raghavarya2002@users.noreply.github.com>
2022-05-04 20:28:20 +0530

    Update two_sum.java

--- leetcode/two_sum.java
+++ leetcode/two_sum.java
@@ -1,3 +1,13 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].    
+
+
+
+
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
