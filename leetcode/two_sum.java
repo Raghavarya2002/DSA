be0e1bf660c510f5dea1a6fee3f44945a0336b12
Raghav <raghavarya2002@gmail.com>
2022-05-04 20:26:09 +0530

    Leetcode journey starts

---
+++ leetcode/two_sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int[] result = new int[2];
+        
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        
+        for(int i =0;i< nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                result[1] = i;
+                result[0] = map.get(target-nums[i]);
+                return result;
+            }
+             map.put(nums[i],i);
+        }
+       
+        return result;
+    }
+}
