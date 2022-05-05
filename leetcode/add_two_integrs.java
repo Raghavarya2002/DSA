58a4869f3209c16898b98bf8ee415aafd88b42cf
Raghav arya <68361729+Raghavarya2002@users.noreply.github.com>
2022-05-05 18:15:49 +0530

    Create add_two_integrs.java

---
+++ leetcode/add_two_integrs.java
@@ -0,0 +1,8 @@
+// Given two integers num1 and num2, return the sum of the two integers.
+
+class Solution {
+    public int sum(int num1, int num2) {
+        int n3 = num1+num2;
+        return n3;
+    }
+}
