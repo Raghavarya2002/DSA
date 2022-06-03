d16b8b22a495c038774228c312ec77c859f41d83
Raghav arya <raghavarya2002@gmail.com>
2022-06-03 14:49:52 +0530

    Create size_of_generic_tree.java

---
+++ Trees/size_of_generic_tree.java
@@ -0,0 +1,77 @@
+// 1. You are given a partially written GenericTree class.
+// 2. You are required to complete the body of size function. The function is expected to count the number of nodes in the tree and return it.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static int size(Node node){
+   int size =0;
+   
+   for(Node child : node.children){
+       int childsize = size(child);
+       size += childsize;
+   }
+   
+   size += 1;
+   return size;
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Node root = construct(arr);
+    int sz = size(root);
+    System.out.println(sz);
+    // display(root);
+  }
+
+}
