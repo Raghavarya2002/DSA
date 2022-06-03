a1e3e5b969fa7db180795478f84523989214eaa9
Raghav <raghavarya2002@gmail.com>
2022-06-03 15:18:34 +0530

    Maximum in generic tree

--- Trees/maximum_in_generic_tree.java
+++ Trees/maximum_in_generic_tree.java
@@ -1,6 +1,3 @@
-// 1. You are given a partially written GenericTree class.
-// 2. You are required to complete the body of max function. The function is expected to find the node with maximum value and return it.
-
 import java.io.*;
 import java.util.*;
 
