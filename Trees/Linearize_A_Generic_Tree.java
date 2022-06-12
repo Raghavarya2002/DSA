15b201e0f66bac9323d5057676ca85bf2b0c9d86
Raghav arya <raghavarya2002@gmail.com>
2022-06-12 13:26:28 +0530

    Update Linearize_A_Generic_Tree.java

--- Trees/Linearize_A_Generic_Tree.java
+++ Trees/Linearize_A_Generic_Tree.java
@@ -1,6 +1,6 @@
 // 1. You are given a partially written GenericTree class.
 // 2. You are required to complete the body of linearize function. The function is expected to create a linear tree i.e. every node will have a single child only. 
-
+// this code is of O(n^2) complexity
 
 import java.io.*;
 import java.util.*;
