56a2a089ea1c818aa2e8ef78154cc20e908af78a
Raghav arya <raghavarya2002@gmail.com>
2022-06-13 21:07:44 +0530

    Create node_to_root_path.java

---
+++ Trees/node_to_root_path.java
@@ -0,0 +1,84 @@
+// 1. You are given a partially written GenericTree class.
+// 2. You are required to complete the body of nodeToRootPath function. The function is expected to return in form of linked list the path from element to root, if the element with data is found.
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+ public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+    if(node.data == data){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(node.data);
+        return list;
+    }
+    
+    for(Node child : node.children){
+        ArrayList<Integer> pathtillchild = nodeToRootPath(child,data);
+        if(pathtillchild.size() > 0){
+            pathtillchild.add(node.data);
+            return pathtillchild;
+        }
+    }
+    return new ArrayList<>();
+ }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    int data = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    ArrayList<Integer> path = nodeToRootPath(root, data);
+    System.out.println(path);
+    // display(root);
+  }
+
+}
