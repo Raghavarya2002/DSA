b44cb5c0ec8565d21d9ba6ef6f91b2cf7d554060
Raghav arya <raghavarya2002@gmail.com>
2022-06-25 22:43:07 +0530

    Create Introduction_to_binary_tree.java

---
+++ Binary Tree/Introduction_to_binary_tree.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data,Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right= right;
+        }
+    }
+
+    public static void main(String[] args) {
+       
+
+    }
+}
