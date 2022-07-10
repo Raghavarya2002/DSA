b7e0af95f4fd804d5bf20ba23f62076582ce2a18
Raghav arya <raghavarya2002@gmail.com>
2022-07-10 21:35:00 +0530

    Create 746_Min_Cost_Climbing_Stairs.java

---
+++ leetcode/746_Min_Cost_Climbing_Stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        
+        //dp signifies cost to use each stair case starting the zeroth index
+        int[] dp = new int[cost.length];
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i =2;i< cost.length;i++){
+            dp[i] = cost[i] +  Math.min(dp[i-1] , dp[i-2]);
+        }
+        
+        return Math.min(dp[cost.length-2],dp[cost.length-1]);
+        
+        
+    }
+}
