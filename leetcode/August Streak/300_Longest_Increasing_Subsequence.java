10caa0976ba5f8a5704dd9a683459507a2232723
Raghav arya <raghavarya2002@gmail.com>
2022-08-08 22:54:40 +0530

    Create 300_Longest_Increasing_Subsequence.java

---
+++ leetcode/August Streak/300_Longest_Increasing_Subsequence.java
@@ -0,0 +1,21 @@
+class Solution {
+    //TC: O(n2)
+    // SC : O(n)
+  public int lengthOfLIS(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    int[] dp = new int[nums.length];
+    int max = 1;
+    for (int i = 0; i < nums.length; i++) {
+      dp[i] = 1;
+      for (int j = 0; j < i; j++) {
+        if (nums[j] < nums[i]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+          max = Math.max(dp[i], max);
+        }
+      }
+    }
+    return max;
+  }
+}
