7206cf88e383b99a8e8053c06d9bc1b5c2975331
Raghav arya <raghavarya2002@gmail.com>
2022-08-12 15:38:49 +0530

    Create 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java

---
+++ leetcode/August Streak/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        int curr = root.val;
+        
+        if(curr < p.val && curr < q.val){
+            return lowestCommonAncestor(root.right,p,q);
+            
+        }
+        if(curr > p.val && curr > q.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        
+        return root;
+    }
+}
