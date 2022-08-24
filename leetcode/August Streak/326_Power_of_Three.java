1e354d6c28850aca445960680ef0da1958acfeb6
Raghav arya <raghavarya2002@gmail.com>
2022-08-24 14:41:06 +0530

    Create 326_Power_of_Three.java

---
+++ leetcode/August Streak/326_Power_of_Three.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+      if(n == 0){
+          return false;
+      }
+        if(n == 1){
+            return true;
+        }
+        if(n > 1){
+            return n % 3 == 0 && isPowerOfThree(n/3);
+        }
+        return false;
+    }
+}
