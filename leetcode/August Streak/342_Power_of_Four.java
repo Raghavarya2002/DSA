86835cc78048df01cbc3083a75c750429b3112b1
AdityaBahl <adityabahl12345@gmail.com>
2022-10-08 23:06:38 +0530

    Added Myself

--- leetcode/August Streak/342_Power_of_Four.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public boolean isPowerOfFour(int n) {
-    
-        double ans = Math.log(n)/Math.log(4);
-        return ans == (int)ans;
-        
-    }
-}
--- leetcode/May/add_two_integers.java
+++
@@ -1,8 +0,0 @@
-// Given two integers num1 and num2, return the sum of the two integers.
-
-class Solution {
-    public int sum(int num1, int num2) {
-        int n3 = num1+num2;
-        return n3;
-    }
-}
