3e45fe1b4a5e9fa0db1b5fa34c162db61aaf511b
Sayantan <sayantann2002@gmail.com>
2022-10-08 17:08:31 +0530

    added myself

---
+++ Hacktoberfest2022/Delete_node_in_a_linked_list.cpp
@@ -0,0 +1,24 @@
+// There is a singly-linked list head and we want to delete a node node in it.
+
+#include <bits/stdc++.h>
+using namespace std;
+// Definition for singly-linked list.
+struct ListNode
+ {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+ class Solution
+ {
+ public:
+     void deleteNode(ListNode *node)
+     {
+         swap(node->val, node->next->val);
+         if (node->next->next)
+             deleteNode(node->next);
+         else
+             node->next = nullptr;
+     }
+ };
