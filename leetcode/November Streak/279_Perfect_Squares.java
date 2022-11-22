65ac354f4327bd123e0edc607fb9527e3d55340e
Raghav arya <raghavarya2002@gmail.com>
2022-11-22 23:20:03 +0530

    Create 279_Perfect_Squares.java

---
+++ leetcode/November Streak/279_Perfect_Squares.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numSquares(int n) {
+        int dp[]=new int [n+1];
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2;i<dp.length;i++){
+            int min=Integer.MAX_VALUE;
+            for(int j=1;j*j<=i;j++){
+                min=Math.min(min,dp[i-j*j]);
+            }
+            dp[i]=min+1;
+        }
+        return dp[n];
+    }
+}
