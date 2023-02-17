22395b36a19658cc3a63b5713e8cf05876a1729a
Raghav arya <raghavarya2002@gmail.com>
2023-02-17 23:09:32 +0530

    Create 783. Minimum Distance Between BST Nodes.java

---
+++ leetcode 2023/February/783. Minimum Distance Between BST Nodes.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    int mini=Integer.MAX_VALUE;
+
+    public void find(TreeNode root,ArrayList<Integer>arr){
+    
+    if(root==null){
+        return;
+    }
+    
+    
+    arr.add(root.val);
+    
+    find(root.left,arr);
+    
+    for(int i=arr.size()-2;i>=0;i--){
+    
+        mini=Math.min(mini,Math.abs(root.val-arr.get(i)));
+    }
+    
+    find(root.right,arr);
+    
+    arr.remove(arr.size()-1);
+    }
+
+    public int minDiffInBST(TreeNode root) {
+    ArrayList<Integer>arr=new ArrayList<>();
+    find(root,arr);
+    return mini;    
+    }
+}
